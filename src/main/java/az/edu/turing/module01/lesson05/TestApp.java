6def25c9ae871837bcf5b9911c3f3dcd00bc94a8
Mahammad <quseynovmaqa25@gmail.com>
2024-10-31 17:35:59 +0400

    operations tasks attached

--- src/main/java/az/edu/turing/module01/lesson05/TestApp.java
+++ src/main/java/az/edu/turing/module01/lesson05/TestApp.java
@@ -2,6 +2,23 @@ package az.edu.turing.module01.lesson05;
 
 public class TestApp {
     public static void main(String[] args) {
-        System.out.println("Hellllo");
+        int a = 10;
+
+        a -= 3;
+        System.out.println("a -= 3 əməliyyatı sonrası a-nın dəyəri: " + a);
+
+
+        a++;
+        System.out.println("a++ əməliyyatı sonrası a-nın dəyəri: " + a);
+
+        int b = 8;
+        if (a == b) {
+            System.out.println("a və b bərabərdir.");
+        }
+
+        int c = 5;
+        if (a != c) {
+            System.out.println("a və c fərqlidir.");
+        }
     }
 }
